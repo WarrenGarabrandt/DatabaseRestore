94efe9274d906d0d9ef8408aa9e68ae97517f4b5
Warren Garabrandt <wmgarab@hotmail.com>
2025-02-11 11:00:07 -0600

    Refactor options into a class, and rework source selection behavior.

--- DatabaseRestore/Program.cs
+++ DatabaseRestore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -17,92 +18,207 @@ namespace DatabaseRestore
             public bool Owner { get; set; }
         }
 
+        public enum AutoSourceMode
+        {
+            None = 0,
+            lastcreated = 1,
+            lastmodified = 2
+        }
+
+        private class OptionsClass
+        {
+            // --autosource mode
+            public AutoSourceMode AutoSourceMode { get; set; }
+            // --autosource path
+            public string SourcePath { get; set; }
+            // --source
+            public string SourceFile { get; set; }
+            // -- temp
+            public string TempFile { get; set; }
+            // --serverip
+            public string SQLServerIP { get; set; }
+            // --servername
+            public string SQLServerName { get; set; }
+            // --serverport
+            public string ServerPortString { get; set; }
+            // parsed from --serverport
+            public int ServerPort = 1433;
+            // --database
+            public string DatabaseName { get; set; }
+            // inferred if not --username and --password specified
+            public bool SSPI = true;
+            // --username
+            public string SQLUsername { get; set; }
+            // --password
+            public string SQLPassword { get; set; }
+            // --rights
+            public string UserRightsString { get; set; }
+            // parsed from --rights
+            public List<UserRightItem> UserRights { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            string SourcePath = "";
-            string TempPath = "";
-            string DatabaseName = "";
-            string UserString = "";
-            List<UserRightItem> UserRightsToAssign = new List<UserRightItem>();
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage:");
-                Console.WriteLine("  --source -s <path>        : source database backup file.");
-                Console.WriteLine("  --temp -t <path>          : temporary directory and file name to copy the source to before restoring.");
-                Console.WriteLine("  --serverip -i <ip>        : IP address of the server to connect to.");
-                Console.WriteLine("  --servername -n <name>    : name of the server to connect to.");
-                Console.WriteLine("  --serverport -p <port>    : port of the server to connect to.");
-                Console.WriteLine("  --username -u <username>  : username to connect to SQL server as.");
-                Console.WriteLine("  --password -p <password>  : password to connect to SQL server (INSECURE)");
-                Console.WriteLine("  --database -d <name>      : name of the database to overwrite with this backup.");
-                Console.WriteLine("  --rights -r <userlist>    : list of users/groups and permission to grant to each.");
-                Console.WriteLine();
-                Console.WriteLine("Users List accepts a list of users and group separated by semi-colon (;).");
-                Console.WriteLine("Separate the user or group from the access modifier by a colon (:)");
-                Console.WriteLine("R grants DataReader, W grants DataWriter, O grants Owner.");
-                Console.WriteLine("Example: domain\\UserName:O;domain\\group:R;SQLUser:RW");
-                Console.WriteLine();
-                Console.WriteLine("Only specify --serverip OR --servername, not both. If neither is specified, localhost is assumed.");
-                Console.WriteLine("If no --serverport is specified, 1433 is assume.");
-                Console.WriteLine("If no --username and --password are specified, then integrated (SSPI) authentication will be used.");
-                Console.WriteLine("If no --temp is specified, then will attempt to restore from --source directly.");
-                Console.WriteLine("In that case, User account for SQL server process must have access to the source file.");
-
+                ShowUsage();
                 return;
             }
+            OptionsClass options = null;
+            if (!ParseOptions(args, out options))
+            {
+                return;
+            }
+            if (!CheckOptions(options))
+            {
+                return;
+            }
+            if (!string.IsNullOrEmpty(options.TempFile) && !CleanTemp(options.TempFile))
+            {
+                Console.WriteLine("Failed deleting previous temp file.");
+                return;
+            }
+            if (!string.IsNullOrEmpty(options.TempFile))
+            {
+                if (!CopyFile(options.SourceFile, options.TempFile))
+                {
+                    Console.WriteLine("Failed copying source backup file to temp path.");
+                    return;
+                }
+                // now that we have a copy of the source file at temp, update the source var so that it gets passed to SQL server instead of the specified source file
+                options.SourceFile = options.TempFile;
+            }
+            if (!RestoreDatabase(TempPath, DatabaseName, UserRightsToAssign))
+            {
+                Console.WriteLine("Failed restoring the database.");
+                return;
+            }
+            if (!string.IsNullOrEmpty(options.TempFile) && !CleanTemp(options.TempFile))
+            {
+                Console.WriteLine("Failed deleting temp file.");
+                return;
+            }
+            Console.WriteLine("Operation Completed.");
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  --autosource -a <mode> <path> : select a source file from specified path based on specified mode.");
+            Console.WriteLine("  --source -s <filepath>        : source database backup file.");
+            Console.WriteLine("  --temp -t <filepath>          : temporary directory and file name to copy the source to before restoring.");
+            Console.WriteLine("  --serverip -i <ip>            : IP address of the server to connect to.");
+            Console.WriteLine("  --servername -n <name>        : name of the server to connect to.");
+            Console.WriteLine("  --serverport -p <port>        : port of the server to connect to.");
+            Console.WriteLine("  --database -d <name>          : name of the database to overwrite with this backup.");
+            Console.WriteLine("  --username -u <username>      : username to connect to SQL server as.");
+            Console.WriteLine("  --password -p <password>      : password to connect to SQL server (INSECURE)");
+            Console.WriteLine("  --rights -r <userlist>        : list of users/groups and permission to grant to each.");
+            Console.WriteLine();
+            Console.WriteLine("Autosource mode specifies which file to choose in the specified directory.");
+            Console.WriteLine(" lastcreated : selects the newest file by creation date");
+            Console.WriteLine(" lastmodified : selects the newest file by modified date");
+            Console.WriteLine("Example: --autosource lastcreated c:\\backups\\database_20241231.bak");
+            Console.WriteLine();
+            Console.WriteLine("Users List accepts a list of users and group separated by semi-colon (;).");
+            Console.WriteLine("Separate the user or group from the access modifier by a colon (:)");
+            Console.WriteLine("R grants DataReader, W grants DataWriter, O grants Owner.");
+            Console.WriteLine("Example: domain\\UserName:O;domain\\group:R;SQLUser:RW");
+            Console.WriteLine();
+            Console.WriteLine("Only specify --serverip OR --servername, not both. If neither is specified, localhost is assumed.");
+            Console.WriteLine("If no --serverport is specified, 1433 is assume.");
+            Console.WriteLine("If no --username and --password are specified, then integrated (SSPI) authentication will be used.");
+            Console.WriteLine("If no --temp is specified, then will attempt to restore from --source directly.");
+            Console.WriteLine("In that case, User account for SQL server process must have access to the source file.");
+            return;
+        }
+
+        private static bool ParseOptions(string[] args, out OptionsClass options)
+        {
+            options = new OptionsClass();
             int pos = 0;
             while (pos < args.Length)
             {
-                if (args[pos].ToLower() == "--source" || args[pos].ToLower() == "-s")
+                if (args[pos].ToLower() == "--autosource" || args[pos].ToLower() == "-a")
                 {
                     pos++;
-                    if (pos < args.Length)
+                    if (pos >=  args.Length)
+                    {
+                        Console.WriteLine("--autosource mode not specified.");
+                        return false;
+                    }
+                    string amode = args[pos];
+                    pos++;
+                    if (pos >= args.Length)
+                    {
+                        Console.WriteLine("--autosource path not specified.");
+                        return false;
+                    }
+                    options.SourcePath = args[pos];
+                    pos++;
+                    switch (amode.ToLower())
+                    {
+                        case "lastcreated":
+                            options.AutoSourceMode = AutoSourceMode.lastcreated;
+                            break;
+                        case "lastmodified":
+                            options.AutoSourceMode = AutoSourceMode.lastmodified;
+                            break;
+                        default:
+                            Console.WriteLine(string.Format("Invalid --autosource mode specified: {0}", amode));
+                            return false;
+                    }
+                }
+                else if (args[pos].ToLower() == "--source" || args[pos].ToLower() == "-s")
+                {
+                    pos++;
+                    if (pos >= args.Length)
                     {
                         Console.WriteLine("Missing Source Path.");
-                        return;
+                        return false;
                     }
-                    SourcePath = args[pos];
+                    options.SourceFile = args[pos];
                     pos++;
                 }
                 else if (args[pos].ToLower() == "--temp" || args[pos].ToLower() == "-t")
                 {
                     pos++;
-                    if (pos < args.Length)
+                    if (pos >= args.Length)
                     {
                         Console.WriteLine("Missing Temp File Name.");
-                        return;
+                        return false;
                     }
-                    TempPath = args[pos];
+                    options.TempFile = args[pos];
                     pos++;
                 }
                 else if (args[pos].ToLower() == "--database" || args[pos].ToLower() == "-d")
                 {
                     pos++;
-                    if (pos < args.Length)
+                    if (pos >= args.Length)
                     {
                         Console.WriteLine("Missing Destination Database Name.");
-                        return;
+                        return false;
                     }
-                    DatabaseName = args[pos];
+                    options.DatabaseName = args[pos];
                     pos++;
                 }
                 else if (args[pos].ToLower() == "--rights" || args[pos].ToLower() == "-r")
                 {
                     pos++;
-                    if (pos < args.Length)
+                    if (pos >= args.Length)
                     {
                         Console.WriteLine("Missing User Accounts to grant permission.");
-                        return;
+                        return false;
                     }
-                    UserString = args[pos];
-                    string[] tempRights = UserString.Split(';');
+                    options.UserRightsString = args[pos];
+                    string[] tempRights = options.UserRightsString.Split(';');
                     foreach (var r in tempRights)
                     {
                         string[] tempItem = r.Split(':');
                         if (tempItem.Length != 2)
                         {
                             Console.WriteLine(string.Format("Unable to parse user permission: {0}", r));
-                            return;
+                            return false;
                         }
                         UserRightItem newRight = new UserRightItem();
                         newRight.Name = tempItem[0];
@@ -118,25 +234,105 @@ namespace DatabaseRestore
                         {
                             newRight.Owner = true;
                         }
-                        UserRightsToAssign.Add(newRight);
+                        options.UserRights.Add(newRight);
                     }
                     pos++;
                 }
                 else
                 {
                     Console.WriteLine(string.Format("Unrecognized command line option {0}", args[pos]));
-                    return;
+                    return false;
                 }
             }
-            if (SourcePath.Length > 0 && System.IO.File.Exists(SourcePath))
+            return true;
+        }
+
+        private static bool CheckOptions(OptionsClass options)
+        {
+            // verify that --autosource and --source weren't specified together
+            if (!string.IsNullOrEmpty(options.SourceFile) && !string.IsNullOrEmpty(options.SourcePath))
             {
-                Console.WriteLine(string.Format("Source database backup: {0}", SourcePath));
+                Console.WriteLine("--autosource and --source were both specified, but only one is needed.");
+                return false;
             }
-            else
+
+            if (!string.IsNullOrEmpty(options.SourcePath) && !System.IO.Directory.Exists(options.SourcePath))
             {
-                Console.WriteLine(string.Format("Can't find source database backup: {0}", SourcePath));
-                return;
+                Console.WriteLine(string.Format("Source path does not exist or access denied: {0}", options.SourcePath));
+                return false;
             }
+            if (!string.IsNullOrEmpty(options.SourcePath))
+            {
+                if (options.AutoSourceMode == AutoSourceMode.None)
+                {
+                    Console.WriteLine("No --autosource mode was specified.");
+                    return false;
+                }
+                options.SourceFile = null;
+                DirectoryInfo dirInfo = new DirectoryInfo(options.SourcePath);
+                if (dirInfo == null || !dirInfo.Exists)
+                {
+                    Console.WriteLine(string.Format("Source path does not exist or access denied: {0}", options.SourcePath));
+                    return false;
+                }
+                FileInfo[] files = dirInfo.GetFiles();
+                DateTime recentCreate = DateTime.MinValue;
+                DateTime recentModify = DateTime.MinValue;
+                FileInfo lastCreated = null;
+                FileInfo lastModified = null;
+                foreach (FileInfo file in files)
+                {
+                    if (file.CreationTime > recentCreate)
+                    {
+                        recentCreate = file.CreationTime;
+                        lastCreated = file;
+                    }
+                    if (file.LastWriteTime > recentModify)
+                    {
+                        recentModify = file.LastWriteTime;
+                        lastModified = file;
+                    }
+                }
+                // a little redundant, but that's because I want to be able to add different filter types in the future and don't know for sure which checks I'll need.
+                if (options.AutoSourceMode == AutoSourceMode.lastcreated)
+                {
+                    if (lastCreated == null)
+                    {
+                        Console.WriteLine(string.Format("Couldn't find the last created file in the directory: {0}", options.SourcePath));
+                        return false;
+                    }
+                    else
+                    {
+                        options.SourceFile = lastCreated.FullName;
+                        Console.WriteLine(string.Format("Autoselected newest source file: {0}", options.SourceFile));
+                    }
+                }
+                else if (options.AutoSourceMode == AutoSourceMode.lastmodified)
+                {
+                    if (lastModified == null)
+                    {
+                        Console.WriteLine(string.Format("Couldn't find the last modified file in the directory: {0}", options.SourcePath));
+                        return false;
+                    }
+                    else
+                    {
+                        options.SourceFile = lastModified.FullName;
+                        Console.WriteLine(string.Format("Autoselected most recently modified source file: {0}", options.SourceFile));
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine(string.Format("Didn't understand the --autosource mode: {0}", options.AutoSourceMode.ToString()));
+                    return false;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(options.SourceFile) && !System.IO.File.Exists(options.SourceFile))
+            {
+                Console.WriteLine(string.Format("Source file does not exist or access denied: {0}", options.SourceFile));
+                return false;
+            }
+
             if (TempPath.Length > 0 && System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(TempPath)))
             {
                 Console.WriteLine(string.Format("Temp Directory: {0}", System.IO.Path.GetDirectoryName(TempPath)));
@@ -192,27 +388,7 @@ namespace DatabaseRestore
                 return;
             }
 
-            if (!CleanTemp(TempPath))
-            {
-                Console.WriteLine("Failed deleting previous temp file.");
-                return;
-            }
-            if (!CopyFile(SourcePath, TempPath))
-            {
-                Console.WriteLine("Failed copying source backup file to temp path.");
-                return;
-            }
-            if (!RestoreDatabase(TempPath, DatabaseName, UserRightsToAssign))
-            {
-                Console.WriteLine("Failed restoring the database.");
-                return;
-            }
-            if (!CleanTemp(TempPath))
-            {
-                Console.WriteLine("Failed deleting temp file.");
-                return;
-            }
-            Console.WriteLine("Operation Completed.");
+            return true;
         }
 
         private static bool CleanTemp(string tempPath)
